e14e072050fe225b510936fb357a0c544285cd26
LukeVanHouten <lukevh@uw.edu>
2023-03-06 23:32:19 -0800

    added stuff for 2nd and 3rd research questions

--- baseball_model.py
+++ baseball_model.py
@@ -7,6 +7,8 @@ from baseball_data import PitchingData
 
 class PitchingModel:
     def __init__(self) -> None:
+        self._all_cy_young_data: pd.DataFrame =\
+            PitchingData().view_merged_data()
         self._train_features: pd.DataFrame = PitchingData().train_features()
         self._train_labels: pd.Series = PitchingData().train_labels()
         self._test_features: pd.DataFrame = PitchingData().test_features()
@@ -15,8 +17,14 @@ class PitchingModel:
     def xgboost_model(self):
         pass
 
-    def random_forest_mode(self):
+    def random_forest_model(self):
         pass
 
     def decision_tree_model(self):
+        pass
+
+    def compare_eras(self):
+        pass
+
+    def starters_vs_relief_pitchers(self):
         pass
