79137835bc91f7bf30fd4db69fbaf655bbadb2fd
LukeVanHouten <lukevh@uw.edu>
2023-03-11 03:37:40 -0800

    questions 2 and 3 done

--- baseball_data.py
+++ baseball_data.py
@@ -1,76 +1,205 @@
 import pybaseball as pb
 import pandas as pd
-import warnings
 from sklearn.model_selection import train_test_split
-
-warnings.simplefilter(action="ignore", category=FutureWarning)
-pd.set_option("display.max_columns", None)
 
 
 class PitchingData:
-    def __init__(self) -> None:
+    '''
+    This class loads in the data used to predict MLB Cy Young Award votes for
+    top-tier pitchers based on their stats for a given season. The class will
+    then return the data split into training and testing features and labels
+    for use in a machine learning model.
+    '''
+    def __init__(self, start_year: int, end_year: int) -> None:
+        '''
+        This method initializes the PitchingData class in order to load in
+        data for stats for baseball pitchers, awards data to find the vote
+        getters for the MLB Cy Young Award for best pitching. The data is then
+        cleaned and split into training and testing features and labels for
+        use in the ML model. The data for the pitcher stats is from FanGraphs,
+        the data for the awards is from the Lahman baseball database, and the
+        data for player IDs is from the Chadwick Bureau of baseball players.
+        '''
+        # Loads in the awards data from the Lahman baseball database method
+        # from the bypaseball module
         awards = pb.lahman.awards_share_players()
+        # Filters the awards to be only for the Cy Young award, and selects
+        # the columns for the player, year, and amount of points won as well
+        # as the amount that are possible to win
         cy_young_votes = awards[awards["awardID"] == "Cy Young"]
         cy_young_votes = cy_young_votes.loc[:, ["yearID", "playerID",
                                                 "pointsWon", "pointsMax"]]
+        # Creates a new column that is the percentage of the points share each
+        # player for a given year got for the Cy Young award
         cy_young_votes["points_share"] = cy_young_votes["pointsWon"] /\
             cy_young_votes["pointsMax"]
+        # Creates a new column that standardizes this points share to the
+        # maximum amount of points ever possible for a Cy Young award
         cy_young_votes["standardized_points"] =\
             cy_young_votes["points_share"] * max(cy_young_votes["pointsMax"])
-
-        pitching_stats_1956_1985 = pb.pitching_stats(1956, 1985, qual=30)
-        pitching_stats_1986_2016 = pb.pitching_stats(1986, 2016, qual=30)
-        pitching_stats = pd.concat([pitching_stats_1956_1985,
-                                    pitching_stats_1986_2016])
+        # Because of how many years there are for the Cy Young award, we need
+        # to parse through the data in two separate calls. Here we create a
+        # value for the middle year of the range by find the difference
+        # between the start and end years, dividing this in half, rounding it
+        # up to the nearest year, and subtracting it from the end year
+        middle_year = end_year - round((end_year - start_year) / 2)
+        # Calls the pybaseball method to get all of the FanGraphs pitching
+        # stats per player for the first half of our date range. The keyword
+        # argument here represents the minimum number of innings pitched in
+        # order for a player to be qualified for the dataset. This value is 38
+        # in order to reduce the time it takes to access the data, as this is
+        # the least amount of innings that any player who has receieved a Cy
+        # Young award vote has pitched.
+        pitching_stats_1st = pb.pitching_stats(start_year, middle_year,
+                                               qual=38)
+        # Checks to see if the start year is less than the end year, and calls
+        # the method to access the FanGraphs data again for the second half of
+        # the year range. Gives an empty Pandas dataframe if the year values
+        # are the same (meaning that only one year of data is to be returned),
+        # as we only need to call the method once to do this.
+        if start_year < end_year:
+            pitching_stats_2nd = pb.pitching_stats(middle_year + 1, end_year,
+                                                   qual=38)
+        else:
+            pitching_stats_2nd = pd.DataFrame()
+        # Combines the two dataframes from the pitching data method calls into
+        # one Pandas dataframe
+        pitching_stats = pd.concat([pitching_stats_1st, pitching_stats_2nd])
         pitching_stats = pitching_stats.loc[:, ["Name", "Season", "IDfg", "W",
                                                 "L", "G", "GS", "ERA", "WHIP",
                                                 "FIP", "IP", "SO", "K%",
                                                 "K/9", "HR/9", "BB/9", "WAR"]]
-        pitching_stats["win_percentage"] = pitching_stats["W"] /\
-            (pitching_stats["W"] + pitching_stats["L"])
-
+        # Access the data for the player IDs. Because many players are from
+        # Latin America and have accents in their names, we encode this data
+        # in the latin-1 encoding in order to prevent any errors
         player_ids = pd.read_csv("players.csv", encoding="latin-1")
+        # Five players in this data do not have baseball-reference IDs that
+        # match up with those from the Lahman database. These players are: J.R
+        # Richard, Freddy Garcia, Johan Santana, CC Sabathia, and R.A. Dickey.
+        # Their IDs have been replaced with the ones found on each pitcher's
+        # respective Baseball-Reference page
         bad_ids = ["richaj.01", "garcifr03", "santajo02", "sabatc.01",
                    "dicker.01"]
         good_ids = ["richajr01", "garcifr02", "santajo01", "sabatcc01",
                     "dickera01"]
         for j, k in zip(bad_ids, good_ids):
             player_ids.loc[player_ids["key_bbref"] == j, "key_bbref"] = k
+        # Selects only the Baseball-reference and FanGraphs IDs. The former is
+        # used by the Lahman database, and the latter is used by the FanGraphs
+        # pitching data
         player_ids = player_ids.loc[:, ["key_bbref", "key_fangraphs"]]
-
+        # Merges these IDs to the Cy Young award data by the
+        # Baseball-Reference IDs
         cy_young_ids = cy_young_votes.merge(player_ids, left_on="playerID",
                                             right_on="key_bbref")
-
+        # Merges the Cy Young award data to the FanGraphs pitching data by the
+        # Fangraphs IDs
         merged_data = pitching_stats.merge(cy_young_ids,
                                            left_on=["IDfg", "Season"],
                                            right_on=["key_fangraphs",
                                                      "yearID"])
+        # Removes columns that are not necessary for the analysis portion
         self._cy_young_vote_getters: pd.DataFrame =\
             merged_data.drop(columns=["IDfg", "yearID", "playerID",
                                       "pointsWon", "pointsMax", "key_bbref",
                                       "points_share", "key_fangraphs"])
-
+        # Selects all of the columns that are features, which will be every
+        # pitching stat excluding losses as well as the feature, the points
+        # for the Cy Young award
         features = self._cy_young_vote_getters.\
-            loc[:, ~self._cy_young_vote_getters.
-                columns.isin(["Name", "Season", "L", "G", "GS",
-                              "standardized_points"])]
+            drop(columns=["Name", "Season", "L", "G", "GS", "IP",
+                          "standardized_points"])
+        # Selects the column that is the label, which are the points for the
+        # Cy Young award
         labels = self._cy_young_vote_getters["standardized_points"]
-
+        # Splits the testing and training labels, with 20% of the data being
+        # testing data, with this being randomized
         self._train_features, self._test_features, self._train_labels,\
             self._test_labels = train_test_split(features, labels,
                                                  test_size=0.2, shuffle=True)
 
     def view_merged_data(self) -> pd.DataFrame:
+        '''
+        This method returns all of the data to be used in the model, for the
+        purposes of perusing the data for information and incites.
+        '''
         return self._cy_young_vote_getters
 
     def train_features(self) -> pd.DataFrame:
+        '''
+        This method returns the features for our training data for our model.
+        '''
         return self._train_features
 
     def train_labels(self) -> pd.DataFrame:
+        '''
+        This method returns the label for our training data for our model,
+        which is the amount of Cy Young points awarded to a given pitcher.
+        This is standardized to 224 points, which is the maximum awarded for
+        any year. Different points are awarded to different votes; a first
+        place vote will give more points than a fifth place vote.
+
+        '''
         return self._train_labels
 
     def test_features(self) -> pd.DataFrame:
+        '''
+        This method returns the features for our testing data for our model.
+        '''
         return self._test_features
 
     def test_labels(self) -> pd.DataFrame:
+        '''
+        This method returns the label for our testing data for our model.
+        '''
         return self._test_labels
+
+    def before_1990(self):
+        '''
+        This method returns all vote getters in the year range prior to the
+        year 1990.
+        '''
+        # We filter out the same rows as we did for the features for the
+        # model, although we keep the points for our linear regression.
+        return self._cy_young_vote_getters[self._cy_young_vote_getters
+                                           ["Season"] < 1990].\
+            drop(columns=["Name", "Season", "L", "G", "GS", "IP"])
+
+    def after_1989(self):
+        '''
+        This method returns all vote getters in the year range after and
+        including the year 1990.
+        '''
+        # We filter out the same rows as we did for the features for the
+        # model, although we keep the points for our linear regression.
+        return self._cy_young_vote_getters[self._cy_young_vote_getters
+                                           ["Season"] >= 1990].\
+            drop(columns=["Name", "Season", "L", "G", "GS", "IP"])
+
+    def starting_pitchers(self):
+        '''
+        This method returns the data for all starting pitchers in the date
+        range. A starting pitcher is a pitcher whose games played includes a
+        substantial amount of starts.
+        '''
+        # We filter out the same rows as we did for the features for the
+        # model, although we keep the points for our linear regression.
+        return self._cy_young_vote_getters[self._cy_young_vote_getters["GS"]
+                                           > 3].drop(columns=["Name", "IP",
+                                                              "Season", "L",
+                                                              "G", "GS"])
+
+    def relief_pitchers(self):
+        '''
+        This method returns the data for all relief pitchers in the date
+        range. A relief pitcher is a pitcher who typically comes in later in
+        the game as opposed to starting it. Because there are times when
+        relief pitchers may end up starting a game for one inning, we have
+        defined relief pitchers to include pitchers who have at most 3 starts.
+        '''
+        # We filter out the same rows as we did for the features for the
+        # model, although we keep the points for our linear regression.
+        return self._cy_young_vote_getters[self._cy_young_vote_getters["GS"]
+                                           <= 3].drop(columns=["Name", "IP",
+                                                               "Season", "L",
+                                                               "G", "GS"])
--- baseball_model.py
+++ baseball_model.py
@@ -1,25 +1,34 @@
-import numpy as np
 import pandas as pd
 import xgboost as xgb
-from matplotlib import pyplot as plt
+from scipy.stats import pearsonr
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from matplotlib import pyplot as plt
 from baseball_data import PitchingData
 
 
 class PitchingModel:
-    def __init__(self) -> None:
-        self._all_cy_young_data: pd.DataFrame =\
-            PitchingData().view_merged_data()
-        self._train_features: pd.DataFrame = PitchingData().train_features()
-        self._train_labels: pd.Series = PitchingData().train_labels()
-        self._test_features: pd.DataFrame = PitchingData().test_features()
-        self._test_labels: pd.Series = PitchingData().test_labels()
+    '''
+    .
+    '''
+    def __init__(self, start_year: int, end_year: int) -> None:
+        '''
+        .
+        '''
+        pitching = PitchingData(start_year, end_year)
+        self._train_features: pd.DataFrame = pitching.train_features()
+        self._train_labels: pd.Series = pitching.train_labels()
+        self._test_features: pd.DataFrame = pitching.test_features()
+        self._test_labels: pd.Series = pitching.test_labels()
+        self._before_1990: pd.DataFrame = pitching.before_1990()
+        self._after_1989: pd.DataFrame = pitching.after_1989()
+        self._starting_pitchers: pd.DataFrame = pitching.starting_pitchers()
+        self._relief_pitchers: pd.DataFrame = pitching.relief_pitchers()
 
-    def xgboost_model(self):
+    def xgboost_model(self) -> float:
         xgb_r = xgb.XGBRegressor(n_estimators=1000, max_depth=10, eta=0.1,
                                  subsample=1.0, colsample_bytree=1.0)
         xgb_r.fit(self._train_features, self._train_labels)
@@ -27,55 +36,245 @@ class PitchingModel:
         error = mean_absolute_error(self._test_labels, vote_pred)
         return error
 
-    def random_forest_model(self):
+    def random_forest_model(self) -> float:
         regressor = RandomForestRegressor(n_estimators=100, random_state=0)
         regressor.fit(self._train_features, self._train_labels)
         vote_pred = regressor.predict(self._test_features)
         error = mean_absolute_error(self._test_labels, vote_pred)
+        perm_importance = permutation_importance(regressor,
+                                                 self._train_features,
+                                                 self._train_labels)
+        forest_importances = pd.Series(perm_importance.importances_mean,
+                                       index=self._test_features.
+                                       columns).sort_values(ascending=False)
+        plt.title("Random Forest Feature Importance")
+        forest_importances.plot.bar(yerr=perm_importance.importances_std)
+        plt.tight_layout()
+        plt.show()
         return error
 
-    def decision_tree_model(self):
+    def decision_tree_model(self) -> float:
         model = DecisionTreeRegressor()
         model.fit(self._train_features, self._train_labels)
         vote_pred = model.predict(self._test_features)
         error = mean_absolute_error(self._test_labels, vote_pred)
         return error
 
-    def compare_eras(self):
+    def compare_eras(self) -> None:
+        '''
+        .
+        '''
+        _, axes = plt.subplots(nrows=4, ncols=5, figsize=(15, 13))
+        axes = axes.ravel()
 
-        old_data = self._all_cy_young_data[self._all_cy_young_data['Season'] > 2010]
-        new_data = self._all_cy_young_data[self._all_cy_young_data['Season'] >= 1990]
-        regressor = RandomForestRegressor(n_estimators=100, random_state=0)
-        old_data_process = old_data.\
-            loc[:, ~old_data.
-                columns.isin(["Name", "Season", "L", "G", "GS",
-                              "standardized_points"])]
-        old_labels = old_data["standardized_points"]
-        new_data_process = old_data.\
-            loc[:, ~new_data.
-                columns.isin(["Name", "Season", "L", "G", "GS",
-                              "standardized_points"])]
-        new_labels = new_data["standardized_points"]
-        old_train_features, old_test_features, old_train_labels, \
-            old_test_labels = train_test_split(old_data_process, old_labels,
-                                                 test_size=0.2, shuffle=True)
-        regressor.fit(old_train_features, old_train_labels)
-        vote_pred = regressor.predict(old_test_features)
-        error = mean_absolute_error(old_test_labels, vote_pred)
-        perm_importance  = permutation_importance(regressor, old_train_features, old_train_labels)
-        forest_importances = pd.Series(perm_importance.importances_mean, index=old_data.columns)
+        statistics_before_1990 = []
+        for i, j in enumerate(self._before_1990.columns[:-1]):
+            regressor_before_1990 = LinearRegression()
+            regressor_before_1990.\
+                fit(self._before_1990[[j]],
+                    self._before_1990[self._train_labels.name])
+            predictions_before_1990 = regressor_before_1990.\
+                predict(self._before_1990[[j]])
 
-        #sorted_indices = np.argsort(importances)[::-1]
-        plt.title('Feature Importance')
-        fig, ax = plt.subplots()
+            axes[i].scatter(self._before_1990[j],
+                            self._before_1990[self._train_labels.name])
+            axes[i].plot(self._before_1990[j], predictions_before_1990,
+                         color="red")
+            axes[i].set_xlabel(j)
+            axes[i].set_title("Vote Points Before 1990 vs. " + j)
 
-        forest_importances.plot.bar(yerr=perm_importance.importances_std, ax=ax)
+            r_before_1990, _ =\
+                pearsonr(self._before_1990[j],
+                         self._before_1990[self._train_labels.name])
+            r2_before_1990 = r_before_1990 ** 2
+            slope_before_1990 = regressor_before_1990.coef_[0]
+            y_intercept_before_1990 = regressor_before_1990.intercept_
+            statistics_before_1990.\
+                append({"Pitching Stat": j,
+                        "Correlation Coefficient": round(r_before_1990, 2),
+                        "R^2": round(r2_before_1990, 2),
+                        "Slope": round(slope_before_1990, 2),
+                        "Y-Intercept": round(y_intercept_before_1990, 2)})
+
+        statistics_after_1989 = []
+        for m, n in enumerate(self._after_1989.columns[:-1]):
+            regressor_after_1989 = LinearRegression()
+            regressor_after_1989.\
+                fit(self._after_1989[[n]],
+                    self._after_1989[self._train_labels.name])
+            predictions_after_1989 = regressor_after_1989.\
+                predict(self._after_1989[[n]])
+
+            axes[m + 10].scatter(self._after_1989[n],
+                                 self._after_1989[self._train_labels.name])
+            axes[m + 10].plot(self._after_1989[n], predictions_after_1989,
+                              color='red')
+            axes[m + 10].set_xlabel(n)
+            axes[m + 10].set_title("Vote Points After 1989 vs. " + n)
+
+            r_after_1989, _ =\
+                pearsonr(self._after_1989[n],
+                         self._after_1989[self._train_labels.name])
+            r2_after_1989 = r_after_1989 ** 2
+            slope_after_1989 = regressor_after_1989.coef_[0]
+            y_intercept_after_1989 = regressor_after_1989.intercept_
+            statistics_after_1989.\
+                append({"Pitching Stat": n,
+                        "Correlation Coefficient": round(r_after_1989, 2),
+                        "R^2": round(r2_after_1989, 2),
+                        "Slope": round(slope_after_1989, 2),
+                        "Y-Intercept": round(y_intercept_after_1989, 2)})
+
         plt.tight_layout()
+        # plt.savefig("compare_eras.png", bbox_inches="tight")
         plt.show()
 
+        regression_statistics_differences = [
+            {"Pitching Stat": a["Pitching Stat"],
+             "Correlation Coefficient Difference": round(
+                a["Correlation Coefficient"] - b["Correlation Coefficient"], 2
+             ),
+             "R^2 Difference": round(a["R^2"] - b["R^2"], 2),
+             "Slope Difference": round(abs(a["Slope"]) - abs(b["Slope"]), 2),
+             "Y-Intercept Difference": round(
+                a["Y-Intercept"] - b["Y-Intercept"], 2
+             )}
+            for b, a in zip(statistics_before_1990, statistics_after_1989)
+            if a["Pitching Stat"] == b["Pitching Stat"]
+        ]
 
-    def starters_vs_relief_pitchers(self):
-        pass
+        print("Linear regression statistics for pitchers before 1990:")
+        for x in statistics_before_1990:
+            x["Line"] = str(x["Slope"]) + "X + " + str(x["Y-Intercept"])
+            if "+ -" in x["Line"]:
+                x["Line"] = x["Line"].replace("+ -", "- ")
+            del x["Slope"]
+            del x["Y-Intercept"]
+            print(str(x)[18:-1].replace("'", '').replace(",", " -", 1))
+        print()
 
+        print("Linear regression statistics for pitchers 1990 and after:")
+        for y in statistics_after_1989:
+            y["Line"] = str(y["Slope"]) + "X + " + str(y["Y-Intercept"])
+            if "+ -" in y["Line"]:
+                y["Line"] = y["Line"].replace("+ -", "- ")
+            del y["Slope"]
+            del y["Y-Intercept"]
+            print(str(y)[18:-1].replace("'", '').replace(",", " -", 1))
+        print()
 
-PitchingModel().xgboost_model()
+        print("Differences between linear regression statistics for pitchers\
+ before 1990 and after 1989:")
+        for z in regression_statistics_differences:
+            print(str(z)[18:-1].replace("'", '').replace(",", " -", 1))
+        return None
+
+    def starters_vs_relievers(self) -> None:
+        '''
+        .
+        '''
+        _, axes = plt.subplots(nrows=4, ncols=5, figsize=(15, 13))
+        axes = axes.ravel()
+
+        statistics_starters = []
+        for i, j in enumerate(self._starting_pitchers.columns[:-1]):
+            regressor_starters = LinearRegression()
+            regressor_starters.\
+                fit(self._starting_pitchers[[j]],
+                    self._starting_pitchers[self._train_labels.name])
+            predictions_starters = regressor_starters.\
+                predict(self._starting_pitchers[[j]])
+
+            axes[i].scatter(self._starting_pitchers[j],
+                            self._starting_pitchers[self._train_labels.name])
+            axes[i].plot(self._starting_pitchers[j],
+                         predictions_starters, color="red")
+            axes[i].set_xlabel(j)
+            axes[i].set_title("Vote Points for Starters vs. " + j)
+
+            r_starters, _ =\
+                pearsonr(self._starting_pitchers[j],
+                         self._starting_pitchers[self._train_labels.name])
+            r2_starters = r_starters ** 2
+            slope_starters = regressor_starters.coef_[0]
+            y_intercept_starters = regressor_starters.intercept_
+            statistics_starters.\
+                append({"Pitching Stat": j,
+                        "Correlation Coefficient": round(r_starters, 2),
+                        "R^2": round(r2_starters, 2),
+                        "Slope": round(slope_starters, 2),
+                        "Y-Intercept": round(y_intercept_starters, 2)})
+
+        statistics_relievers = []
+        for m, n in enumerate(self._relief_pitchers.columns[:-1]):
+            regressor_relievers = LinearRegression()
+            regressor_relievers.\
+                fit(self._relief_pitchers[[n]],
+                    self._relief_pitchers[self._train_labels.name])
+            predictions_relievers = regressor_relievers.\
+                predict(self._relief_pitchers[[n]])
+
+            axes[m + 10].\
+                scatter(self._relief_pitchers[n],
+                        self._relief_pitchers[self._train_labels.name])
+            axes[m + 10].plot(self._relief_pitchers[n],
+                              predictions_relievers, color='red')
+            axes[m + 10].set_xlabel(n)
+            axes[m + 10].set_title("Vote Points for Relievers vs. " + n)
+
+            r_relievers, _ =\
+                pearsonr(self._relief_pitchers[n],
+                         self._relief_pitchers[self._train_labels.name])
+            r2_relievers = r_relievers ** 2
+            slope_relievers = regressor_relievers.coef_[0]
+            y_intercept_relievers = regressor_relievers.intercept_
+            statistics_relievers.\
+                append({"Pitching Stat": n,
+                        "Correlation Coefficient": round(r_relievers, 2),
+                        "R^2": round(r2_relievers, 2),
+                        "Slope": round(slope_relievers, 2),
+                        "Y-Intercept": round(y_intercept_relievers, 2)})
+
+        plt.tight_layout()
+        plt.savefig("compare_eras.png", bbox_inches="tight")
+        # plt.show()
+
+        starters_vs_relievers_statistics_differences = [
+            {"Pitching Stat": a["Pitching Stat"],
+             "Correlation Coefficient Difference": round(
+                a["Correlation Coefficient"] - b["Correlation Coefficient"], 2
+             ),
+             "R^2 Difference": round(a["R^2"] - b["R^2"], 2),
+             "Slope Difference": round(abs(a["Slope"]) - abs(b["Slope"]), 2),
+             "Y-Intercept Difference": round(
+                a["Y-Intercept"] - b["Y-Intercept"], 2
+             )}
+            for b, a in zip(statistics_starters, statistics_relievers)
+            if a["Pitching Stat"] == b["Pitching Stat"]
+        ]
+
+        print("Linear regression statistics for starting pitchers:")
+        for x in statistics_starters:
+            x["Line"] = str(x["Slope"]) + "X + " + str(x["Y-Intercept"])
+            if "+ -" in x["Line"]:
+                x["Line"] = x["Line"].replace("+ -", "- ")
+            del x["Slope"]
+            del x["Y-Intercept"]
+            print(str(x)[18:-1].replace("'", '').replace(",", " -", 1))
+        print()
+
+        print("Linear regression statistics for relief pitchers:")
+        for y in statistics_relievers:
+            y["Line"] = str(y["Slope"]) + "X + " + str(y["Y-Intercept"])
+            if "+ -" in y["Line"]:
+                y["Line"] = y["Line"].replace("+ -", "- ")
+            del y["Slope"]
+            del y["Y-Intercept"]
+            print(str(y)[18:-1].replace("'", '').replace(",", " -", 1))
+        print()
+
+        print("Differences between linear regression statistics for starters\
+ and relievers:")
+        for z in starters_vs_relievers_statistics_differences:
+            print(str(z)[18:-1].replace("'", '').replace(",", " -", 1))
+        return None
